df9900a40feddcb29e852f2b9f534f1e6c06d86f
Qiuyang <1206857991@qq.com>
2022-06-26 16:57:12 +0800

    项目环境搭建

---
+++ src/test/java/cn/qy/store/StoreApplicationTests.java
@@ -0,0 +1,24 @@
+package cn.qy.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+class StoreApplicationTests {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    public void getConnection() throws SQLException {
+        System.out.println(dataSource.getConnection());
+
+    }
+
+}
