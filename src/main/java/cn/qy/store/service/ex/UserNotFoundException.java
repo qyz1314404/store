c202c1d27f522d30fb9c6f24dd6bbb5c61edfb14
Qiuyang <1206857991@qq.com>
2022-07-10 19:46:43 +0800

    用户登录-持久层&业务层异常添加

---
+++ src/main/java/cn/qy/store/service/ex/UserNotFoundException.java
@@ -0,0 +1,28 @@
+package cn.qy.store.service.ex;
+
+/**
+ * @description: 用户名数据不存在的异常
+ * @author: QiuYang
+ * @time: 2022/7/7
+ */
+public class UserNotFoundException extends ServiceException {
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
