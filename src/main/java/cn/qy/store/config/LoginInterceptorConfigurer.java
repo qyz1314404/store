3814d17d4988b45e7a029e5e4b9c7528743c7604
Qiuyang <1206857991@qq.com>
2022-07-13 07:35:35 +0800

    用户登录-拦截器

---
+++ src/main/java/cn/qy/store/config/LoginInterceptorConfigurer.java
@@ -0,0 +1,41 @@
+package cn.qy.store.config;
+
+import cn.qy.store.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 处理器拦截器的注册
+ * @author: QiuYang
+ * @time: 2022/7/12
+ */
+@Configuration
+public class LoginInterceptorConfigurer implements WebMvcConfigurer {
+    /*配置拦截器*/
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //    创建自定义的拦截器对象
+        HandlerInterceptor interceptor=new LoginInterceptor();
+        //配置白名单：存放在一个List集合
+        List<String> patterns = new ArrayList<>();
+        patterns.add("/bootstrap3/**");
+        patterns.add("/css/**");
+        patterns.add("/images/**");
+        patterns.add("/js/**");
+        patterns.add("/web/register.html");
+        patterns.add("/web/login.html");
+        patterns.add("/web/index.html");
+        patterns.add("/web/product.html");
+        patterns.add("/users/reg");
+        patterns.add("/users/login");
+
+
+        //完成拦截器的注册
+        registry.addInterceptor(interceptor).addPathPatterns("/**").excludePathPatterns(patterns);
+    }
+}
